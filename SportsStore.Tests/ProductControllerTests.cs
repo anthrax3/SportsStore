13d67bf7441a4de273405c832c4a05c2e2d4bd4a
Ryan Transfiguracion <ryan_transfiguracion@outlook.com>
2017-08-23 23:12:36 -0700

    Adding pagination - partially done.

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+
+
+  public class ProductControllerTests
+  {
+
+    [Fact]
+    public void Can_Paginate()
+    {
+      // Arrange
+      Mock<IProductRepository> mock = new Mock<IProductRepository>();
+      mock.Setup(m => m.Products).Returns(new Product[] {
+
+      });
+    }
+  }
+}
