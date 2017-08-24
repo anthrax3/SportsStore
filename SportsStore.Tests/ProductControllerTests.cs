ef353abe0a45b373d4b24a7917e1be778552b478
Ryan Transfiguracion <rtransfig@gmail.com>
2017-08-24 11:31:18 -0700

    Added unit test for pagination integrity.  Added custom tag helper class for creating pagination links.

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -18,8 +18,24 @@ namespace SportsStore.Tests
       // Arrange
       Mock<IProductRepository> mock = new Mock<IProductRepository>();
       mock.Setup(m => m.Products).Returns(new Product[] {
-
+        new Product { ProductID = 1, Name = "P1" },
+        new Product { ProductID = 1, Name = "P2" },
+        new Product { ProductID = 1, Name = "P3" },
+        new Product { ProductID = 1, Name = "P4" },
+        new Product { ProductID = 1, Name = "P5" }
       });
+
+      ProductController controller = new ProductController(mock.Object);
+      controller.PageSize = 3;
+
+      // Act
+      IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+      // Assert
+      Product[] prodArray = result.ToArray();
+      Assert.True(prodArray.Length == 2);
+      Assert.Equal("P4", prodArray[0].Name);
+      Assert.Equal("P5", prodArray[1].Name);
     }
   }
 }
